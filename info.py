60b0e1cf7f375bf2339637908e43a265d9e2b6c2
Mohamad Sabeh <mohamadsabeh1234@gmail.com>
2025-09-23 12:07:52 +0300

    modified the info python file with my personal information

---
+++ info.py
@@ -0,0 +1,6 @@
+name = "Mohamad Hassan Sabeh"
+
+reason ="Im in interested in further developing my skills in programming and learning new coding languages, with the added benifit of helping me achieve more in the company "
+
+print(name)
+print(reason)
